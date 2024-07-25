81f19a74fdf22d35841404b587302e8a41c208fb
Marian Figula <majofigulam3@gmail.com>
2024-07-25 10:10:04 +0200

    feat: functionality to show content when scrolling

--- script/script.js
+++ script/script.js
@@ -12,3 +12,24 @@ cvButton.addEventListener('click', () => {
     link.download = 'CV.pdf';
     link.dispatchEvent(new MouseEvent('click'));
 })
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     const sections = document.querySelectorAll("section");
+//
+//     const options = {
+//         threshold: 0.25
+//     };
+//
+//     const observer = new IntersectionObserver((entries, observer) => {
+//         entries.forEach(entry => {
+//             if (entry.isIntersecting) {
+//                 entry.target.classList.add("show");
+//                 observer.unobserve(entry.target); // Stop observing once it’s in view
+//             }
+//         });
+//     }, options);
+//
+//     sections.forEach(section => {
+//         observer.observe(section);
+//     });
+// });
