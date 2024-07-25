1ca1ef98c40933c7bf1d4c51058ddeba0d58ce35
Marian Figula <majofigulam3@gmail.com>
2024-07-25 09:24:19 +0200

    feat: functionality to download pdf

--- script/script.js
+++ script/script.js
@@ -1,5 +1,14 @@
 
-const typed = new Typed('#welcome', {
-    strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+const typed = new Typed('#typed-text', {
+    strings: ['Web Developer', '.'],
     typeSpeed: 100,
 });
+
+const cvButton = document.getElementById("download-cv");
+cvButton.addEventListener('click', () => {
+    console.log("clicked");
+    const link = document.createElement('a');
+    link.href = 'assets/CV.pdf';
+    link.download = 'CV.pdf';
+    link.dispatchEvent(new MouseEvent('click'));
+})
