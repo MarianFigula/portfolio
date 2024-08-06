5aa97a9c18944ea10fd11660274caf6949495201
Marian Figula <majofigulam3@gmail.com>
2024-08-06 09:57:47 +0200

    feat: sending email using email js and adding comments

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 const cvButton = document.getElementById("download-cv");
-
+const form = document.getElementById("form");
 
 const typedStringsEN = [
     "Web Developer",
@@ -22,6 +22,7 @@ const typed = new Typed('#typed-text', {
     typeSpeed: 70,
 });
 
+// button for downloading cv
 cvButton.addEventListener('click', () => {
     console.log("clicked");
     const link = document.createElement('a');
@@ -30,6 +31,7 @@ cvButton.addEventListener('click', () => {
     link.dispatchEvent(new MouseEvent('click'));
 })
 
+// navbar icons
 document.querySelector('.icon-container').addEventListener('click', () => {
     const listIcon = document.getElementById('list-icon');
     const xIcon = document.getElementById('x-icon');
@@ -40,7 +42,35 @@ document.querySelector('.icon-container').addEventListener('click', () => {
     nav.classList.toggle('show');
 });
 
+// scroll down icon
 document.querySelector('.scroll-down-icon').addEventListener('click', () => {
     const about = document.querySelector('#about');
     about.scrollIntoView({behavior: 'smooth'});
 })
+
+// sending email with emailjs from contact form
+form.addEventListener('submit', async function (event) {
+    event.preventDefault();
+
+    const formData = {
+        name : form.name.value,
+        email : form.email.value,
+        message : form.message.value
+    }
+
+    await emailjs.send("service_ueenyhf", "template_v3mrdc5",
+        {
+            from_name: formData.name,
+            from_email: formData.email,
+            message: formData.message
+        })
+        .then(function(response) {
+            console.log("response:", response.status, response.text);
+            alert('Email sent successfully!');
+            form.reset();
+        }, function(error) {
+            console.log("error:", error)
+            alert('Failed to send email. Please try again later.');
+        });
+
+});
